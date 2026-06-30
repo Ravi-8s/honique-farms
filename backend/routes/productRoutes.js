2c1c2f83d5324efed6a0b6cf6e01667348de3ac9
Ravi-8s <ravishankarfrom1998@gmail.com>
2026-06-30 20:52:01 +0530

    feat: implement products GET and POST APIs

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
+
 const router = express.Router();
 
 const {
   getProducts,
+  createProduct,
 } = require("../controllers/ProductController");
 
 router.get("/", getProducts);
+
+router.post("/", createProduct);
 
 module.exports = router;
